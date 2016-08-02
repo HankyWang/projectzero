64cbe3e1cdffcb2bb2e02191e7998c5b403fc279
hankvistawang <hankvistawang@yahoo.com>
2016-08-02 03:54:47 +0000

    new

---
+++ 1/imooc.php
@@ -0,0 +1,13 @@
+<?php
+    $token = 'weixin';
+    $timestamp = $_GET['timestamp'];
+    $nonce = $_GET['nonce'];
+    $signature = $_GET['signature'];
+    $array = array($token, $timestamp, $nonce);
+    $tmpstr = implode('', $array);
+    $tmpstr = sha1($tmpstr);
+    if ($tmpstr == $signature){
+        echo "imooc";
+        exit();
+    }
+?>
