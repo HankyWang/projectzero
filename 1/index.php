95d5d131a2892dd61f0e42bcfef5e8a7bb0ee2b4
hankvistawang <hankvistawang@yahoo.com>
2016-08-02 02:08:14 +0000

    fix a bug

--- 1/index.php
+++ 1/index.php
@@ -6,7 +6,7 @@
     $array = array($timestamp, $nonce, $token);
     sort($array);
     $tmpstr = implode('', $array);
-    $tmpstr = sha1(tmpstr);
+    $tmpstr = sha1($tmpstr);
     if ($tmpstr == $signature){
         echo $_GET['echostr'];
         exit;
