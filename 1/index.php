5f1e849a087d0f84951ea8b80e1b000be18d10cf
hankvistawang@yahoo.com <hankvistawang@yahoo.com@95606195-fa5d-46ea-b29f-416946eb5d08>
2016-08-02 01:54:12 +0000

    edit

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,13 @@
 <?php
-echo '<strong>Welcome to SAE!</strong>';
+    $timestamp = $_GET['timestamp'];
+    $nonce = $_GET['nonce'];
+    $token = 'weixin';
+    $signature = $_GET['signature'];
+    $array = array($timestamp, $nonce, $token);
+    sort( $array );
+    $tmpstr  = implode('', $array);
+    $tmpstr = sha1($tmpstr);
+    if ($tmpstr == $signature){
+        echo $_GET['echostr'];
+        exit;
+    }
