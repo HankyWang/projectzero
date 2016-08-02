9e440f757d602cd3697e4edaec599f77c0eaabac
hankvistawang@yahoo.com <hankvistawang@yahoo.com@95606195-fa5d-46ea-b29f-416946eb5d08>
2016-08-02 01:55:46 +0000

    edit0

--- 1/index.php
+++ 1/index.php
@@ -1,13 +1,2 @@
 <?php
-    $timestamp = $_GET['timestamp'];
-    $nonce = $_GET['nonce'];
-    $token = 'weixin';
-    $signature = $_GET['signature'];
-    $array = array($timestamp, $nonce, $token);
-    sort( $array );
-    $tmpstr  = implode('', $array);
-    $tmpstr = sha1($tmpstr);
-    if ($tmpstr == $signature){
-        echo $_GET['echostr'];
-        exit;
-    }
+    echo 'mooc'
