62391e8d848db8175f50aa6bc900f563afcb3127
hankvistawang <hankvistawang@yahoo.com>
2016-08-02 02:05:20 +0000

    edit to weixin

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,13 @@
 <?php
-    echo 'mooc';
+    $timestamp = $_GET['timestamp'];
+    $token = 'weixin';
+    $nonce = $_GET['nonce'];
+    $signature = $_GET['signature'];
+    $array = array($timestamp, $nonce, $token);
+    sort($array);
+    $tmpstr = implode('', $array);
+    $tmpstr = sha1(tmpstr);
+    if ($tmpstr == $signature){
+        echo $_GET['echostr'];
+        exit;
+    }
