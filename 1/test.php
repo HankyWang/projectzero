04d0ad577230c2d16b407b732f84a1007aeabe73
hankvistawang <hankvistawang@yahoo.com>
2016-08-03 02:45:19 +0000

    init

---
+++ 1/test.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+</head>
+<body>
+    <p>
+    <?php 
+        echo 'just try out';
+    ?>
+    </p>
+</body>
+</html>
